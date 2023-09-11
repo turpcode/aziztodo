90a89b9514e59bec063dc4d68de83341db5824db
azizsigar <aazizsigar@gmail.com>
2023-09-11 18:00:06 +0200

    v3

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect("mongodb+srv://aazizsigar:1234@aziz.fjez7sn.mongodb.net/?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://aziz:1234@aziz.fjez7sn.mongodb.net/?retryWrites=true&w=majority")
 .then(()=>{
     console.log('DB on')
 })
